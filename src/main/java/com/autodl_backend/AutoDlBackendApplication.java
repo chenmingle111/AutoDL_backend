050e46b52952a45d174d944ce5330958f41fb306
Miles <2120451464@qq.com>
2025-11-19 09:47:10 +0800

    init: project created
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/autodl_backend/AutoDlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.autodl_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoDlBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoDlBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/autodl_backend/AutoDlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autodl_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoDlBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
