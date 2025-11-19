566fc89e6509131bd79da5d64ec60918a72c81c7
yankehu <joechon31@gmail.com>
2025-11-19 10:41:23 +0800

    新建了controller和pojo两个包

--- src/main/java/com/autodl_backend/controller/PathVariableController.java
+++ src/main/java/com/autodl_backend/controller/PathVariableController.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.autodl_backend.demos.web;
+package com.autodl_backend.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/autodl_backend/pojo/User.java
+++ src/main/java/com/autodl_backend/pojo/User.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.autodl_backend.demos.web;
+package com.autodl_backend.pojo;
 
 /**
  * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
