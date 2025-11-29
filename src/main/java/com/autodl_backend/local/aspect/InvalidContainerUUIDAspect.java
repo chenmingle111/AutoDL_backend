f0accd16c5ed50dcc0bc638f9ed00d42808cb4ae
yankehu <joechon31@gmail.com>
2025-11-29 16:18:13 +0800

    创建了aop的框架

--- src/main/java/com/autodl_backend/local/aspect/InvalidContainerUUIDAspect.java
+++ src/main/java/com/autodl_backend/local/aspect/InvalidContainerUUIDAspect.java
@@ -1,4 +1,29 @@
 package com.autodl_backend.local.aspect;
 
+import com.autodl_backend.local.pojo.response.ApiResponse;
+import com.autodl_backend.local.pojo.response.ResponseCode;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
 public class InvalidContainerUUIDAspect {
+
+    /**
+     * 捕获ContainerUUID异常并且转换为恰当的http响应
+     */
+    @Around("execution(* com.autodl_backend.local.service.impl..*.*(..))")
+    public Object HandleContainerUUIDException(ProceedingJoinPoint joinPoint) throws Throwable {
+        try {
+            return joinPoint.proceed();
+        } catch (Exception e) {
+            log.error("ContainerUUID异常:{}", e.getMessage());
+            ApiResponse.error(ResponseCode.ERROE, e.getMessage());
+        }
+        return null;
+    }
 }
--- src/main/java/com/autodl_backend/local/aspect/InvalidImageUUIDApsect.java
+++ src/main/java/com/autodl_backend/local/aspect/InvalidImageUUIDApsect.java
@@ -1,4 +1,30 @@
 package com.autodl_backend.local.aspect;
 
+
+import com.autodl_backend.local.pojo.response.ApiResponse;
+import com.autodl_backend.local.pojo.response.ResponseCode;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
 public class InvalidImageUUIDApsect {
+
+    /**
+     * 捕获ImageUUID异常并且转换为恰当的http响应
+     */
+    @Around("execution(* com.autodl_backend.local.service.impl..*.*(..))")
+    public Object HandleImageUUIDException(ProceedingJoinPoint joinPoint) throws Throwable {
+        try {
+            return joinPoint.proceed();
+        } catch (Exception e) {
+            log.error("ImageUUID异常:{}", e.getMessage());
+            ApiResponse.error(ResponseCode.ERROE, e.getMessage());
+        }
+        return null;
+    }
 }
